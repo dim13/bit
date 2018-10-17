fe9b2a43b05bbfa4424e117f2b20053c8185a49a
Dimitri Sokolyuk <demon@dim13.org>
2018-10-17 23:05:00 +0200

    cleanup

--- bit.go
+++ bit.go
@@ -1,43 +1,43 @@
 // Package bit implements variable-size bit-fields
 package bit
 
-// Field is a variable bit-field
+// Field is a variable-length bit-field
 type Field []byte
 
 // IsSet checks if bit n is set
 func (bf *Field) IsSet(n int) bool {
-	bit, off := n>>3, 1<<uint(n&7)
+	bit, off := n>>3, byte(1<<uint(n&7))
 	if len(*bf) <= bit {
 		return false
 	}
-	return (*bf)[bit]&byte(off) != 0
+	return (*bf)[bit]&off != 0
 }
 
 // Set bit n
 func (bf *Field) Set(n int) *Field {
-	bit, off := n>>3, 1<<uint(n&7)
+	bit, off := n>>3, byte(1<<uint(n&7))
 	if len(*bf) <= bit {
 		*bf = append(*bf, make([]byte, bit-len(*bf)+1)...)
 	}
-	(*bf)[bit] |= byte(off)
+	(*bf)[bit] |= off
 	return bf
 }
 
-// IsClr checks if bit n is set
-func (bf *Field) IsClr(n int) bool {
-	bit, off := n>>3, 1<<uint(n&7)
+// IsClear checks if bit n is cleared
+func (bf *Field) IsClear(n int) bool {
+	bit, off := n>>3, byte(1<<uint(n&7))
 	if len(*bf) <= bit {
 		return true
 	}
-	return (*bf)[bit]&byte(off) == 0
+	return (*bf)[bit]&off == 0
 }
 
-// Clr bit n
-func (bf *Field) Clr(n int) *Field {
-	bit, off := n>>3, 1<<uint(n&7)
+// Clear bit n
+func (bf *Field) Clear(n int) *Field {
+	bit, off := n>>3, byte(1<<uint(n&7))
 	if len(*bf) <= bit {
 		return bf
 	}
-	(*bf)[bit] &^= byte(off)
+	(*bf)[bit] &^= off
 	return bf
 }
--- bit_test.go
+++ bit_test.go
@@ -5,15 +5,15 @@ import "testing"
 func TestBit(t *testing.T) {
 	bf := new(Field)
 	for i := 0; i < 64; i++ {
-		if bf.IsSet(i) || !bf.IsClr(i) {
+		if bf.IsSet(i) || !bf.IsClear(i) {
 			t.Error("got set, want clear")
 		}
-		bf.Clr(i)
-		if bf.IsSet(i) || !bf.IsClr(i) {
+		bf.Clear(i)
+		if bf.IsSet(i) || !bf.IsClear(i) {
 			t.Error("got set, want clear")
 		}
 		bf.Set(i)
-		if bf.IsClr(i) || !bf.IsSet(i) {
+		if bf.IsClear(i) || !bf.IsSet(i) {
 			t.Error("got clear, want set")
 		}
 	}
